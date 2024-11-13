6b0264e8baa7f337da8adb4df5db051679c953c3
seizaku.dev <seizaku.dev@gmail.com>
2024-11-14 04:55:03 +0800

    utils: combine date and time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -5,11 +6,29 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export async function fileToBase64(file: File): Promise<string> {
+export function fileToBase64(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.readAsDataURL(file);
+
     reader.onload = () => resolve(reader.result as string);
-    reader.onerror = (error) => reject(error);
+    reader.onerror = reject;
+
+    reader.readAsDataURL(file);
   });
+}
+
+export function combineDateAndTime(date: Date, time: string) {
+  // Extract hours and minutes from the time string
+  const [timePart, modifier] = time.split(" ");
+  let [hours, minutes] = timePart!.split(":").map(Number);
+
+  // Adjust hours for AM/PM format
+  if (modifier === "PM" && hours! < 12) hours! += 12;
+  if (modifier === "AM" && hours === 12) hours = 0;
+
+  // Set the hours and minutes to the selected date
+  const combinedDate = new Date(date);
+  combinedDate.setHours(hours!, minutes, 0, 0); // set seconds and milliseconds to 0
+
+  return combinedDate;
 }
