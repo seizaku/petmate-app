8de5aa80b5c69967bf1686f72f4ef4c810036a4b
seizaku.dev <seizaku.dev@gmail.com>
2024-11-14 04:51:29 +0800

    feat: business view

---
+++ src/app/user/business/[...businessId]/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { AppContainer, AppNavbar, AppBottomNav } from "~/components/app";
+import { Business } from "~/features/business/components/business";
+
+interface BusinessPage {
+  params: {
+    businessId: string[];
+  };
+}
+
+export default async function BusinessPage({
+  params,
+}: {
+  params: { businessId: Promise<string[]> };
+}) {
+  const id = await params.businessId;
+
+  return (
+    <AppContainer>
+      <AppNavbar title="Service Provider" href="/user/home" />
+
+      <Business businessId={id[0]!} />
+    </AppContainer>
+  );
+}
