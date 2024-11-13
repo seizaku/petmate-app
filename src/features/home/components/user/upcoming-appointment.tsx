afdb37b79535d028651758ceb21a313e72bb234a
seizaku.dev <seizaku.dev@gmail.com>
2024-11-14 04:50:00 +0800

    feat: upcoming appointment card

---
+++ src/features/home/components/user/upcoming-appointment.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "~/components/ui/card";
+import { useFindFirstUser } from "~/lib/hooks";
+
+const UpcomingAppointment = () => {
+  const router = useRouter();
+  const { data: user } = useFindFirstUser({
+    include: {
+      appointments: {
+        include: {
+          business: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <Card className="bg-gradient mb-4 bg-gradient-to-r from-primary via-primary/90 to-primary/70 text-white">
+      <CardHeader>
+        <CardTitle>Upcoming Appointment</CardTitle>
+      </CardHeader>
+      {user?.appointments.length ? (
+        <>
+          <CardContent>
+            <h1 className="text-2xl font-bold">
+              {user?.appointments?.[0]?.datetime.toDateString()}
+            </h1>
+            <p>{user?.appointments?.[0]?.business.businessName}</p>
+          </CardContent>
+          <CardFooter>
+            <Button
+              onClick={() => {
+                router.push(`/appointments/${user?.appointments?.[0]?.id}`);
+              }}
+              variant={"outline"}
+              className="text-foreground"
+            >
+              Details
+            </Button>
+          </CardFooter>
+        </>
+      ) : (
+        <CardContent>
+          <p>You have no appointments yet.</p>
+        </CardContent>
+      )}
+    </Card>
+  );
+};
+
+export { UpcomingAppointment };
