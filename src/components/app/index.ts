63352d1ec2d7a6aa08a24cd455c0dfaf8cf383a4
seizaku.dev <seizaku.dev@gmail.com>
2024-11-13 17:17:42 +0800

    feat: main app components

---
+++ src/components/app/index.ts
@@ -0,0 +1,6 @@
+import { AppContainer } from "./app-container";
+import { AppLoader } from "./app-loader";
+import { AppNavbar } from "./app-navbar";
+
+
+export { AppContainer, AppLoader, AppNavbar}
