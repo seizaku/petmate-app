945f8dd2a11c15fcff51b2b23f5e95128a58f690
seizaku.dev <seizaku.dev@gmail.com>
2024-11-14 04:52:32 +0800

    feat: add bottom nav component

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -1,6 +1,6 @@
 import { AppContainer } from "./app-container";
 import { AppLoader } from "./app-loader";
 import { AppNavbar } from "./app-navbar";
+import { AppBottomNav } from "./app-bottom-nav";
 
-
-export { AppContainer, AppLoader, AppNavbar}
+export { AppContainer, AppLoader, AppNavbar, AppBottomNav}
