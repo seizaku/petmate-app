e1338e81a4756a1c436212e4c5c867e0182617db
seizaku.dev <seizaku.dev@gmail.com>
2024-11-15 09:55:33 +0800

    feat: notifications

---
+++ src/app/business/notifications/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { FaBell } from "react-icons/fa6";
+import { useFindFirstUser } from "~/lib/hooks";
+import { AppBottomNav, AppContainer, AppNavbar } from "~/components/app";
+import { format } from "date-fns";
+
+export default function NotificationsPage() {
+  const { data: user } = useFindFirstUser({
+    include: {
+      business: {
+        include: {
+          notifications: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <AppContainer>
+      <AppNavbar title="Notifications" href="/business/home" />
+
+      <div className="grid gap-2">
+        {user?.business?.notifications
+          .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+          .map((notification) => (
+            <Card key={notification.id} className="shadow-none">
+              <CardHeader>
+                <div className="flex items-center gap-4">
+                  <Button variant={"secondary"} size={"icon"}>
+                    <FaBell className="text-primary" />
+                  </Button>
+                  <div className="space-y-1">
+                    <CardTitle>{notification.businessMessage}</CardTitle>
+                    <CardDescription className="text-xs">
+                      {format(notification.createdAt, "MMMM dd, yyyy hh:mm a")}
+                    </CardDescription>
+                  </div>
+                </div>
+              </CardHeader>
+            </Card>
+          ))}
+      </div>
+
+      <AppBottomNav />
+    </AppContainer>
+  );
+}
---
+++ src/app/user/notifications/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { FaBell } from "react-icons/fa6";
+import { useFindFirstUser } from "~/lib/hooks";
+import { AppBottomNav, AppContainer, AppNavbar } from "~/components/app";
+import { format } from "date-fns";
+
+export default function NotificationsPage() {
+  const { data: user } = useFindFirstUser({
+    include: {
+      notifications: true,
+    },
+  });
+
+  return (
+    <AppContainer>
+      <AppNavbar title="Notifications" href="/user/home" />
+
+      <div className="grid gap-2">
+        {user?.notifications
+          .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+          .map((notification) => (
+            <Card key={notification.id} className="shadow-none">
+              <CardHeader>
+                <div className="flex items-center gap-4">
+                  <Button variant={"secondary"} size={"icon"}>
+                    <FaBell className="text-primary" />
+                  </Button>
+                  <div className="space-y-1">
+                    <CardTitle>{notification.userMessage}</CardTitle>
+                    <CardDescription className="text-xs">
+                      {format(notification.createdAt, "MMMM dd, yyyy hh:mm a")}
+                    </CardDescription>
+                  </div>
+                </div>
+              </CardHeader>
+            </Card>
+          ))}
+      </div>
+
+      <AppBottomNav />
+    </AppContainer>
+  );
+}
