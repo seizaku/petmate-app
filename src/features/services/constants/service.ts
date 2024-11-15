76126fb675a1512b991c4cc968f5b2066ff166e7
seizaku.dev <seizaku.dev@gmail.com>
2024-11-15 20:28:45 +0800

    feat: services

---
+++ src/features/services/constants/service.ts
@@ -0,0 +1,72 @@
+export const vetServices = {
+  grooming: [
+    "Grooming",
+    "Bathing",
+    "Hair Trimming",
+    "Nail Clipping",
+    "Ear Cleaning",
+  ],
+  boarding: [
+    "Pet Boarding",
+    "Overnight Stay",
+    "Daycare Services",
+  ],
+  consultation: [
+    "Veterinary Consultation",
+    "General Health Check-up",
+    "Second Opinion Consultation",
+  ],
+  medicalCare: [
+    "Emergency Care",
+    "Vaccination",
+    "Spaying/Neutering",
+    "Dental Care",
+    "Microchipping",
+    "Parasite Control",
+    "Senior Pet Care",
+    "Specialized Treatments",
+    "Diagnostic Services",
+  ],
+  wellness: [
+    "Dietary and Nutritional Guidance",
+    "Weight Management Programs",
+    "Wellness Packages",
+    "Health Screening",
+  ],
+  reproductiveServices: [
+    "Reproductive Services",
+    "Breeding Assistance",
+    "Pregnancy Ultrasounds",
+    "Postnatal Care",
+  ],
+  endOfLife: [
+    "End-of-Life Services",
+    "Hospice Care",
+    "Euthanasia",
+    "Cremation or Burial Services",
+  ],
+  trainingAndBehavior: [
+    "Pet Training",
+    "Obedience Training",
+    "Behavioral Counseling",
+    "Anxiety Management",
+    "Aggression Training",
+  ],
+  petCareAndSupplies: [
+    "Pet Sitting",
+    "Pet Supplies",
+    "Pet Travel Services",
+  ],
+  rehabilitationAndTherapy: [
+    "Rehabilitation and Therapy",
+    "Physical Therapy",
+    "Hydrotherapy",
+    "Acupuncture",
+  ],
+  exoticPetCare: [
+    "Exotic Pet Care",
+    "Birds",
+    "Reptiles",
+    "Small Mammals (Rabbits, Guinea Pigs, etc.)",
+  ],
+};
